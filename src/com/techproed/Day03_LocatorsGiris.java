573c46318cd86a3edd87ad07944232048c434706
unknown <ayyldzbesra@gmail.com>
2020-09-10 14:30:20 +0300

    day03ders sonu

--- src/com/techproed/Day03_LocatorsGiris.java
+++ src/com/techproed/Day03_LocatorsGiris.java
@@ -6,30 +6,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 public class Day03_LocatorsGiris {
+     
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\asus\\Documents\\Selenium dependencies\\drives\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
-        // bu kod sasyfanin yuklenmesi icin sure verdik 3000 mmilisaniye (3sn) oncesinde yuklenirse beklemeyi birakiyor
+        // bu kod sasyfanin yuklenmesi icin sure verdik 1000 mmilisaniye (1sn) oncesinde yuklenirse beklemeyi birakiyor
+
         driver.manage().window().maximize();
-        driver.get("http://a.testaddressbook.com/sign_in");
-//ilk web elementimizi bulacagiz
-        String name = "Hamza" ; //
-        // web sayfasindaki elementlerin weri tipi   WebElement   tir syi da olsa  metin de olsa hepsi icin gecerli
-//webelementimizi id kullanarak bulduk
+        driver.get("http://a.testaddressbook.com/");
+        //ilk web elementimizi bulacagiz
+
         WebElement signInLink = driver.findElement(By.id("sign-in"));
-        //bu kod la sign elementini bulduk
-/*
-<a id="sign-in" class="nav-item nav-link" data-test="sign-in" href="/sign_in">Sign in</a>
- */
+        // web sayfasindaki elementlerin weri tipi   WebElement   tir sayi da olsa  metin de olsa resim de olsa hepsi icin gecerli
+        // webelementimizi id kullanarak bulduk
+        // bu kod la sign elementini bulduk
+        // <a id="sign-in" class="nav-item nav-link" data-test="sign-in" href="/sign_in">Sign in</a>
+
         signInLink.click();
-//bu kodla sayfaya girip  sign in tusuna  kodla  tiklamis olsuk
+        //bu kodla sayfaya girip  sign in tusuna  kodla  tiklamis olsuk
         /*
         <input type="email" class="form-control" placeholder="Email" data-test="email"
         name="session[email]" id="session_email">
         simdi bu elementi bulmak icin bu elementin ver tipi nedir // id ile bulacagiz
          */
+
         WebElement emailKutusu = driver.findElement(By.id("session_email"));
         emailKutusu.sendKeys("testtechproed@gmail.com");
         // simdi passwort kutusunu bulacagiz id ile ariyoruz
@@ -37,15 +39,19 @@ public class Day03_LocatorsGiris {
         <input class="form-control" placeholder="Password" data-test="password" type="password"
          name="session[password]" id="session_password">
          */
+
         WebElement sifreKutusu = driver.findElement(By.id("session_password"));
-// sifre kutusunun icine parola gonderecegiz
+        // sifre kutusunun icine parola gonderecegiz
         sifreKutusu.sendKeys("Test1234!");
-/*
-<input type="submit" name="commit" value="Sign in" class="btn btn-primary"
- data-test="submit" data-disable-with="Sign in">
- */
-        // sign in butonunu buluyoruz. // name="commit" (burda id olmadigi icin name ile arama yapacagiz)
+        /*
+        <input type="submit" name="commit" value="Sign in" class="btn btn-primary"
+        data-test="submit" data-disable-with="Sign in">
+        */
+        // sign in butonunu buluyoruz.
+        // name="commit" (burda id olmadigi icin name ile arama yapacagiz)
+
         WebElement signInButonu = driver.findElement(By.name("commit"));
+        signInButonu.click();
 
         String baslik = driver.getTitle();
 
@@ -56,7 +62,7 @@ public class Day03_LocatorsGiris {
             System.out.println("Giriş Başarısız.");
         }
 
-        signInButonu.click();
+
 
 
     }
