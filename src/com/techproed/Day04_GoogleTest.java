cf005a8fa8113d7e8bf8393feb1661404a14f51f
unknown <ayyldzbesra@gmail.com>
2020-09-10 17:48:23 +0300

    day04 ders sonu

--- src/com/techproed/Day04_GoogleTest.java
+++ src/com/techproed/Day04_GoogleTest.java
@@ -1,6 +1,9 @@
 package com.techproed;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
@@ -10,6 +13,19 @@ public class Day04_GoogleTest {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
-        
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+        WebElement google = driver.findElement(By.name("q"));
+        google.click();
+        google.sendKeys("city bike");
+        google.submit();
+        WebElement listele = driver.findElement(By.id("gsr"));
+        System.out.println(listele.getText());
+        WebElement shopping = driver.findElement(By.name("q"));
+        shopping.click();
+
+
+
+
     }
 }
--- src/com/techproed/Day04_LocatersGiris.java
+++ src/com/techproed/Day04_LocatersGiris.java
@@ -11,17 +11,24 @@ public class Day04_LocatersGiris {
     public static void main(String[] args) {
 
             System.setProperty("webdriver.chrome.driver","C:\\Users\\asus\\Documents\\Selenium dependencies\\drives\\chromedriver.exe");
-
             WebDriver driver = new ChromeDriver();
-
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
             driver.manage().window().maximize();
             driver.get("http://a.testaddressbook.com/");
 
+            WebElement signiInLink = driver.findElement(By.id("sign-in"));
+            signiInLink.click();
+            WebElement emailKutusu = driver.findElement(By.className("form-control"));
+            emailKutusu.sendKeys("sdasdca@gmail.com");
+            WebElement sifreKutusu = driver.findElement(By.id("session_password"));
+            sifreKutusu.sendKeys("adgshnyrt");
+            WebElement signInButonu = driver.findElement(By.name("commit"));
+            signInButonu.click();
 
-        WebElement signInLink = driver.findElement(By.id("sign-in"));
 
-        signInLink.click();
+
+
+
 
 
     }
